fbe23e68fac99a14a20d7de8f7e38f5a351f90ec
USENGROUP\mm-okuda <mm-okuda@usen-next.jp>
2023-06-02 17:18:42 +0900

    １つだけ表示

--- pokemon.php
+++ pokemon.php
@@ -5,9 +5,25 @@ $response = file_get_contents($url);
 
 $data = json_decode($response, true);
 
-print("<pre>");
-var_dump($data["name"]);
-var_dump($data["sprites"]["front_default"]);
-var_dump($data["height"]);
-var_dump($data["weight"]);
-print("</pre>");
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ポケモン図鑑</title>
+</head>
+<body>
+    <h1>ポケモン図鑑</h1>
+    <p>
+       <img src = <?php echo $data["sprites"]["front_default"]; ?> ><br>
+       名前：<?php echo $data["name"]; ?><br>
+       高さ：<?php echo $data["height"]; ?><br>
+       重さ：<?php echo $data["weight"]; ?>
+    </p>
+    
+    
+    
+</body>
+</html>
