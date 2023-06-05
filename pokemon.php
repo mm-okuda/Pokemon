d2335ee2b9135ef9feaa4aa1c6188b42da6b2a39
mm-okuda <133298867+mm-okuda@users.noreply.github.com>
2023-06-05 15:46:55 +0900

    名前の日本語表示

--- pokemon.php
+++ pokemon.php
@@ -5,6 +5,11 @@ $response = file_get_contents($url);
 
 $data = json_decode($response, true);
 
+$url2 = "https://pokeapi.co/api/v2/pokemon-species/1/";
+$response2 = file_get_contents($url2);
+
+$data2 = json_decode($response2, true);
+
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +23,8 @@ $data = json_decode($response, true);
     <h1>ポケモン図鑑</h1>
     <p>
        <img src = <?php echo $data["sprites"]["front_default"]; ?> ><br>
-       名前：<?php echo $data["name"]; ?><br>
+       名前（英語）：<?php echo $data["name"]; ?><br>
+       名前（日本語）：<?php echo $data2["names"][0]["name"]; ?><br>
        高さ：<?php echo $data["height"]; ?><br>
        重さ：<?php echo $data["weight"]; ?>
     </p>
