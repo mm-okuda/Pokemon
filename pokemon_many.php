7afc7e296b976ce325785b63074d28f7588bcdd3
mm-okuda <133298867+mm-okuda@users.noreply.github.com>
2023-06-05 11:24:53 +0900

    Update pokemon_many.php

--- pokemon_many.php
+++ pokemon_many.php
@@ -1,12 +1,48 @@
 <?php
 
 $url = "https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0";
-$responce = file_get_contents($url);
+$response = file_get_contents($url);
 
 $data = json_decode($response, true);
 
-print("<pre>");
-foreach($data["results"] as $key => $value){
-    var_dump($value["name"]);
-}
-print("</pre>");
+$i = 0;
+
+foreach($data["results"] as $value){
+    //var_dump($data["results"]);
+
+    $response = file_get_contents($value["url"]);
+    $data = json_decode($response, true);
+
+    $name[$i] = $value["name"];
+    $image[$i] = $data["sprites"]["front_default"]; 
+    $height[$i] = $data["height"];
+    $weight[$i] = $data["weight"];
+
+    $i++;
+}       
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ポケモン図鑑</title>
+</head>
+<body>
+    <h1>ポケモン図鑑</h1>
+    <?php
+    for($k=0; $k<10; $k++){
+        echo $name[$k];
+        echo "<br>";
+        echo '<img src = "' . $image[$k] . '">';
+        echo "<br>";
+        echo $height[$k];
+        echo "<br>";
+        echo $weight[$k];
+        echo "<br><br>";
+    }
+    ?>    
+</body>
+</html>
